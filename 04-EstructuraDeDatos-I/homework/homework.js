456339d4bc0702d994d4be5bbf431c9707a53097
Juan Pablo Evangelista <juanpicap23@gmail.com>
2022-08-19 16:24:58 -0300

    estructura de datos1 homework opt

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -22,7 +22,7 @@ function nFactorial(n) {
 }
 
 function nFibonacci(n) {
-  if (n == 0) return 0;
+  if (n <= 0) return 0;
   else if (n == 1) return 1;
   else return nFibonacci(n - 1) + nFibonacci(n - 2);
 }
